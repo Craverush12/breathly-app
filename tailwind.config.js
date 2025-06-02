ebe329a12f73d48a742564d028ca191c9a654ecb
Arjun <104681931+Craverush12@users.noreply.github.com>
2025-06-02 23:51:57 +0530

    ye

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-const { hairlineWidth, platformSelect } = require("nativewind/dist/theme-functions");
-
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -11,15 +9,7 @@ module.exports = {
         "breathly-serif-semibold": ["Lora-SemiBold"],
         "breathly-regular": ["GeneralSans-Regular"],
         "breathly-medium": ["GeneralSans-Medium"],
-        "breathly-mono": [
-          platformSelect({ ios: "HelveticaNeue-Light", android: "sans-serif-thin" }),
-        ],
-      },
-      borderWidth: {
-        hairline: hairlineWidth(),
-      },
-      spacing: {
-        hairline: hairlineWidth(),
+        "breathly-mono": ["HelveticaNeue-Light"],
       },
     },
   },
